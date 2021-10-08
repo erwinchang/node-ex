947fab6b37b49f471f555df68776ce0b7469efae
erwin <m927216@mail.ntust.edu.tw>
2021-10-07 23:19:17 -0700

    add drawLine

--- public/js/main.js
+++ public/js/main.js
@@ -11,4 +11,57 @@ $(document).ready(function() {
 		console.log('data',data)
 		$('#member_msg').append('<div>' + data.msg + '</div>');
 	})
+
+	$(document).on('mousedown', '#whiteboard', function(e){
+		e.preventDefault();
+		drawing = true
+
+		offset = $(e.currentTarget).offset();
+		x = e.pageX - offset.left
+		y = e.pageY - offset.top
+		drawLine(x,y,x+1,y+1)
+	})
+
+	$(document).on('mouseup', '#whiteboard', function(e){
+		e.preventDefault();
+		drawing = false
+	})
+
+	$(document).on('mousemove', '#whiteboard', function(e){
+		e.preventDefault();
+		
+		if(drawing){
+			offset = $(e.currentTarget).offset();
+			new_x = e.pageX - offset.left
+			new_y = e.pageY - offset.top
+			drawLine(x,y,new_x,new_y)
+			x = new_x
+			y = new_y
+		}
+	})
+
+	$(document).on('change', '#size', function(e){
+		ctx.lineWidth = $(this).val()
+	})
+
+	var c = document.getElementsByTagName('canvas')[0]
+	c.width  = 648
+	c.height = 770
+	var drawing = false
+
+	var ctx = c.getContext('2d')
+	ctx.lineCap = 'round'
+	ctx.lineJoin = 'round'
+	ctx.strokeStyle = '#000000'
+	ctx.lineWidth = 1
+	var offset, x, y, new_x, new_y;
+
+	function drawLine(x, y, new_x, new_y)
+	{ 
+			ctx.beginPath()
+			ctx.moveTo(x, y)
+			ctx.lineTo(new_x, new_y)
+			ctx.closePath()
+			ctx.stroke()
+	}	
 })
