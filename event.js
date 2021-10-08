6cf0b12766f33fb9bb4271442bce94b72efcbc00
erwin <m927216@mail.ntust.edu.tw>
2021-10-07 20:28:52 -0700

    patch leaveBoard msg

--- event.js
+++ event.js
@@ -3,6 +3,10 @@ const Room1 = 'general'
 
 const leaveBoard = (socket) => () => {
 	console.log('Socket.io disconnect:', socket.id)
+	let obj = new Object
+	obj.msg = socket.name
+	obj.msg = socket.name + ' 已離開'
+	socket.broadcast.emit('msg', obj)
 }
 
 const loginBoard = (socket) => (data) => {
@@ -12,8 +16,13 @@ const loginBoard = (socket) => (data) => {
 	let obj = new Object
 	obj.name = data.name
 	obj.msg = data.name + '已上線'
+
+	socket.name = data.name
 	
 	socket.emit('msg',obj)
+
+	//To all connected clients except the sender
+	socket.broadcast.emit('msg', obj)
 }
 
 
