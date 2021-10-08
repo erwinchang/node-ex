1916ea9a4e7c6ee0402963aa4b54ee1d61e669b2
erwin <m927216@mail.ntust.edu.tw>
2021-10-07 19:09:34 -0700

    add socker.on msg

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,14 @@
-const socket = io.connect()
+$(document).ready(function() {
+	const socket = io.connect()
 
-socket.on('connect', function () {
-	let obj = new Object
-	obj.name = prompt('尊姓大名？')
-	socket.emit('login',obj)
-})
+	socket.on('connect', function () {
+		let obj = new Object
+		obj.name = prompt('尊姓大名？')
+		socket.emit('login',obj)
+	})
 
-socket.on('msg', function(data) {
-	console.log('data',data)
+	socket.on('msg', function(data) {
+		console.log('data',data)
+		$('#member_msg').append('<div>' + data.msg + '</div>');
+	})
 })
