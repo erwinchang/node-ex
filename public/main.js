4eca93b975a7ff22b26e5c75973ee3461b37b905
erwin <m927216@mail.ntust.edu.tw>
2021-10-14 02:12:05 -0700

    patch message

--- public/main.js
+++ public/main.js
@@ -1,6 +1,21 @@
 const socket = io.connect()
 
+//container
+//|--inbox
+//|   |-inbox_people...........inboxPeople
+//|   |-inbox_message
+//|   |  |-messages_history....messageBox
+//|   |  |-fallback............fallback
+//|
+//|--mssage_form...............messageForm
+//|   |-message_form_input.....inputField
+//|   |-message_form_button
+
 const inboxPeople = document.querySelector('.inbox__people')
+const inputField = document.querySelector(".message_form__input")
+const messageForm = document.querySelector(".message_form")
+const messageBox = document.querySelector(".messages__history")
+const fallback = document.querySelector(".fallback")
 
 let userName = ''
 
@@ -31,15 +46,66 @@ const addToUsersBox = (userName) => {
 	console.log(userBox)
 
   inboxPeople.innerHTML += userBox
-};
+}
+
+const addNewMessage = ({ user, message }) => {
+  const time = new Date();
+  const formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric" })
+
+  const receivedMsg = `
+  <div class="incoming__message">
+    <div class="received__message">
+      <p>${message}</p>
+      <div class="message__info">
+        <span class="message__author">${user}</span>
+        <span class="time_date">${formattedTime}</span>
+      </div>
+    </div>
+  </div>`
+
+  const myMsg = `
+  <div class="outgoing__message">
+    <div class="sent__message">
+      <p>${message}</p>
+      <div class="message__info">
+        <span class="time_date">${formattedTime}</span>
+      </div>
+    </div>
+  </div>`
+
+  messageBox.innerHTML += user === userName ? myMsg : receivedMsg;
+}
 
 // new user is created so we generate nickname and emit event
-newUserConnected();
+newUserConnected()
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  if (!inputField.value) {
+    return
+  }
+
+	console.log('submit',{
+    message: inputField.value,
+    nick: userName,		
+	})
+
+  socket.emit("chat message", {
+    message: inputField.value,
+    nick: userName,
+  })
+
+  inputField.value = ""
+})
 
 socket.on("new user", function (data) {
   data.map((user) => addToUsersBox(user));
-});
+})
 
 socket.on("user disconnected", function (userName) {
   document.querySelector(`.${userName}-userlist`).remove();
-});
+})
+
+socket.on("chat message", function (data) {
+  addNewMessage({ user: data.nick, message: data.message })
+})
